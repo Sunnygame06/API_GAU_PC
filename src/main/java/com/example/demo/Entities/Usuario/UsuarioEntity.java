47647d742cc93d74fc8a616e6845ad2558cc84b7
Daniel Alvarado <20210133@ricaldone.edu.sv>
2025-11-11 15:04:46 -0600

    Usuario casi completo

--- src/main/java/com/example/demo/Entities/Usuario/UsuarioEntity.java
+++ src/main/java/com/example/demo/Entities/Usuario/UsuarioEntity.java
@@ -1,4 +1,26 @@
 package com.example.demo.Entities.Usuario;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "USUARIO")
+@Getter @Setter @ToString @EqualsAndHashCode
 public class UsuarioEntity {
+    private Long Id_Usuario;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String unidad;
+    private String pass;
+    private String rol;
+    private String region;
+    private String departamento;
+    private String municipio;
+    private String distrito;
+    private String filtrar;
 }
--- src/main/java/com/example/demo/Models/DTO/Usuario/UsuarioDTO.java
+++ src/main/java/com/example/demo/Models/DTO/Usuario/UsuarioDTO.java
@@ -1,4 +1,39 @@
 package com.example.demo.Models.DTO.Usuario;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class UsuarioDTO {
+    private Long Id_Usuario;
+
+    @NotBlank(message = "El nombre no puede ser nulo")
+    @Size(max = 200, message = "El nombre solo puede tener como maximo 200 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El telefono no puede ser nulo")
+    @Size(max = 9, message = "El telefono solo puede tener como maximo 9 caracteres")
+    private String telefono;
+
+    @NotBlank(message = "El nombre no puede ser nulo")
+    @Email(message = "El formato debe ser el correcto: correo@ejemplo.com")
+    @Size(max = 200, message = "El nombre solo puede tener como maximo 200 caracteres")
+    private String email;
+
+    @NotBlank(message = "La unidad no puede ser nulo")
+    @Size(max = 200, message = "La unidad solo puede tener como maximo 200 caracteres")
+    private String unidad;
+
+    @NotBlank(message = "La contraseña no puede ser nula")
+    @Size(max = 200, message = "La contraseña solo puede tener como maximo 200 caracteres")
+    private String pass;
+    private String rol;
+    private String region;
+    private String departamento;
+    private String municipio;
+    private String distrito;
+    private String filtrar;
 }
